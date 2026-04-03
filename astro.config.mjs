b6e6b8708dbcaae8dd0603033df0df273dd8e038
Philipao0122 <juanku2003@gmail.com>
2026-04-03 17:56:07 -0500

    CloudflareFunctionssv8

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     platform: 'workers',
-    imageService: 'compile'
+    imageService: 'cloudflare'
   }),
   build: {
     assets: 'assets',
